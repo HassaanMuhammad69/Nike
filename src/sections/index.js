f904623025bf43fe169e834d5707f3128e03eaaf
Hassaan Muhammad <hassaans299@gmail.com>
2023-12-13 12:52:58 -0800

    completed

---
+++ src/sections/index.js
@@ -0,0 +1,15 @@
+import Hero from './Hero'
+import CustomerReviews from './CustomerReviews'
+import Services from './Services'
+import SpecialOffers from './SpecialOffers'
+import Subscribe from './Subscribe'
+import SuperQuality from './SuperQuality'
+import Footer from './Footer'
+import PopularProduct from './PopularProduct'
+
+export{
+    Hero,CustomerReviews,Services,SpecialOffers,
+    Subscribe,SuperQuality,Footer,PopularProduct
+}
+
+
